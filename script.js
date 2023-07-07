9b43f3eb48821c2c2749254772c0e63430ea2fee
Tarik <tarikozgur0607@gmail.com>
2023-07-07 09:49:18 +0300

    add new version 0.0.3

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const gameBoard = {
     gameBoard :[],
     player:[],
 } 
+const box = document.getElementsByClassName("box");
+
+
+for (let i = 0 ; i < box.length; i++) {
+    box[i].addEventListener('click' , ()=>{
+        box[i].innerText = "X";
+    }) ; 
+ }
